3a50f0c14ca2c9b11d6ca4d49ae542cbc2bc44b3
Sushant Khanal <dreamerking114@gmail.com>
2017-11-12 13:32:17 +0545

    info window appears on click on list(with knockout)

--- app.js
+++ app.js
@@ -1,135 +1,90 @@
-var map;
+var defaultIcon, highlightedIcon, largeInfowindow, bounds, q;
 var markers = [];
-function initMap() {
-  var styles = [
-    {
-      featureType: 'water',
-      stylers: [
-        { color: '#19a0d8' }
-      ]
-    },{
-      featureType: 'administrative',
-      elementType: 'labels.text.stroke',
-      stylers: [
-        { color: '#ffffff' },
-        { weight: 6 }
-      ]
-    },{
-      featureType: 'administrative',
-      elementType: 'labels.text.fill',
-      stylers: [
-        { color: '#e85113' }
-      ]
-    },{
-      featureType: 'road.highway',
-      elementType: 'geometry.stroke',
-      stylers: [
-        { color: '#efe9e4' },
-        { lightness: -40 }
-      ]
-    },{
-      featureType: 'transit.station',
-      stylers: [
-        { weight: 9 },
-        { hue: '#e85113' }
-      ]
-    },{
-      featureType: 'road.highway',
-      elementType: 'labels.icon',
-      stylers: [
-        { visibility: 'off' }
-      ]
-    },{
-      featureType: 'water',
-      elementType: 'labels.text.stroke',
-      stylers: [
-        { lightness: 100 }
-      ]
-    },{
-      featureType: 'water',
-      elementType: 'labels.text.fill',
-      stylers: [
-        { lightness: -100 }
-      ]
-    },{
-      featureType: 'poi',
-      elementType: 'geometry',
-      stylers: [
-        { visibility: 'on' },
-        { color: '#f0e4d3' }
-      ]
-    },{
-      featureType: 'road.highway',
-      elementType: 'geometry.fill',
-      stylers: [
-        { color: '#efe9e4' },
-        { lightness: -25 }
-      ]
+var locations = [
+  {title: 'Pashupati Nath', location: {lat: 27.7105, lng: 85.3487}},
+  {title: 'Bhaktapur Durbar Square', location: {lat: 27.6721, lng: 85.4283}},
+  {title: 'Patan Durbar Square', location: {lat: 27.6727, lng: 85.3253}},
+  {title: 'Basantapur', location: {lat: 27.7042, lng: 85.3065}},
+  {title: 'Sundarijal', location: {lat: 27.7909, lng: 85.4272}},
+  {title: 'Budhanilkantha', location: {lat: 27.7654, lng: 85.3653}},
+  {title: 'Swayambhu', location: {lat: 27.7148, lng: 85.2903}}          
+]; 
+var styles = [
+  {
+    featureType: 'water',
+    stylers: [
+      { color: '#19a0d8' }
+    ]
+  },{
+    featureType: 'administrative',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      { color: '#ffffff' },
+      { weight: 6 }
+    ]
+  },{
+    featureType: 'administrative',
+    elementType: 'labels.text.fill',
+    stylers: [
+      { color: '#e85113' }
+    ]
+  },{
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [
+      { color: '#efe9e4' },
+      { lightness: -40 }
+    ]
+  },{
+    featureType: 'transit.station',
+    stylers: [
+      { weight: 9 },
+      { hue: '#e85113' }
+    ]
+  },{
+    featureType: 'road.highway',
+    elementType: 'labels.icon',
+    stylers: [
+      { visibility: 'off' }
+    ]
+  },{
+    featureType: 'water',
+    elementType: 'labels.text.stroke',
+    stylers: [
+      { lightness: 100 }
+    ]
+  },{
+    featureType: 'water',
+    elementType: 'labels.text.fill',
+    stylers: [
+      { lightness: -100 }
+    ]
+  },{
+    featureType: 'poi',
+    elementType: 'geometry',
+    stylers: [
+      { visibility: 'on' },
+      { color: '#f0e4d3' }
+    ]
+  },{
+    featureType: 'road.highway',
+    elementType: 'geometry.fill',
+    stylers: [
+      { color: '#efe9e4' },
+      { lightness: -25 }
+    ]
+  }
+];
+
+
+
+var ViewModel = function() {
+    var self = this;
+    this.locationList = ko.observableArray();
+    for(var j = 0; j<locations.length; j++) {
+        this.locationList.push(locations[j]);
     }
-  ];
-
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 27.723875, lng: 85.357847},
-      zoom: 13,
-      styles: styles,
-      mapTypeControl: false            
-  });
-                  
-  //var Pashupati = {lat: 27.7105, lng: 85.3487}
-
-  var locations = [
-    {title: 'Pashupati Nath', location: {lat: 27.7105, lng: 85.3487}},
-    {title: 'Bhaktapur Durbar Square', location: {lat: 27.6721, lng: 85.4283}},
-    {title: 'Patan Durbar Square', location: {lat: 27.6727, lng: 85.3253}},
-    {title: 'Basantapur', location: {lat: 27.7042, lng: 85.3065}},
-    {title: 'Sundarijal', location: {lat: 27.7909, lng: 85.4272}},
-    {title: 'Budhanilkantha', location: {lat: 27.7654, lng: 85.3653}},
-    {title: 'Swayambhu', location: {lat: 27.7148, lng: 85.2903}}          
-  ];        
-        
-  var largeInfowindow = new google.maps.InfoWindow();  
-  var defaultIcon = makeMarkerIcon('0091ff');
-  var highlightedIcon = makeMarkerIcon('FF0000');
-
-  var bounds = new google.maps.LatLngBounds();
-
-  for ( var i = 0; i<locations.length; i++){
-      var position = locations[i].location;
-      var title = locations[i].title;
-      var marker = new google.maps.Marker({
-          position: position,
-          title: title,
-          animation: google.maps.Animation.DROP,
-          icon: defaultIcon,
-          id: i
-      });
-        // Push the marker to our array of markers.
-      markers.push(marker);
-      bounds.extend(markers[i].position);
-
-      marker.addListener('mouseover', function() {
-        this.setIcon(highlightedIcon);
-      });  
-
-      marker.addListener('mouseout', function() {
-        this.setIcon(defaultIcon);
-      });       
-
-      marker.addListener('click',function() {
-        populateInfoWindow(this, largeInfowindow);
-      });
-  }
-
-
-  // Extend the boundaries of the map for each marker and display the marker
-  for (var i = 0; i < markers.length; i++) {
-        markers[i].setMap(map);
-        bounds.extend(markers[i].position);
-  }
-  map.fitBounds(bounds);
-
-
-  function makeMarkerIcon(markerColor) {
+    this.makeMarkerIcon = function(markerColor) {
         var markerImage = new google.maps.MarkerImage(
         'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
         '|40|_|%E2%80%A2',
@@ -138,19 +93,86 @@ function initMap() {
         new google.maps.Point(10, 34),
         new google.maps.Size(21,34));
         return markerImage;
-  }
+    } 
+    this.locateMarker = function(i) {
+        var position = locations[i].location;
+        var title = locations[i].title;
+        var marker = new google.maps.Marker({
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            icon: defaultIcon,
+            id: i
+        });
+          // Push the marker to our array of markers.
+        markers.push(marker);
+        bounds.extend(markers[i].position);
 
-  function populateInfoWindow(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
-    if (infowindow.marker != marker) {
-      infowindow.marker = marker;
-      infowindow.setContent('<div>' + marker.title + '</div>');
-      infowindow.open(map, marker);
-      // Make sure the marker property is cleared if the infowindow is closed.
-      infowindow.addListener('closeclick',function(){
-        infowindow.setMarker = null;
-      });
+        marker.addListener('mouseover', function() {
+          this.setIcon(highlightedIcon);
+        });  
+
+        marker.addListener('mouseout', function() {
+          this.setIcon(defaultIcon);
+        });       
+
+        marker.addListener('click',function() {
+          self.populateInfoWindow(this, largeInfowindow);
+        });    
     }
-  }
+
+    this.populateInfoWindow = function(marker, infowindow) {
+  // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+          infowindow.marker = marker;
+          infowindow.setContent('<div>' + marker.title + '</div>');
+          infowindow.open(map, marker);
+          // Make sure the marker property is cleared if the infowindow is closed.
+          infowindow.addListener('closeclick',function(){
+            infowindow.setMarker = null;
+          });
+        }
+    }
+
+    this.initMap = function() {
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 27.723875, lng: 85.357847},
+            zoom: 13,
+            styles: styles,
+            mapTypeControl: false            
+        });
+             
+
+        largeInfowindow = new google.maps.InfoWindow();  
+        defaultIcon = self.makeMarkerIcon('0091ff');
+        highlightedIcon = self.makeMarkerIcon('FF0000');
+
+        bounds = new google.maps.LatLngBounds();
+        for ( var i = 0; i<locations.length; i++){
+          self.locateMarker(i);
+        }
+
+        // Extend the boundaries of the map for each marker and display the marker
+        for (var i = 0; i < markers.length; i++) {
+              markers[i].setMap(map);
+              bounds.extend(markers[i].position);
+        }
+        map.fitBounds(bounds);
+
+    }
+    this.initMap();
+
+    this.popInfo = function(place) {
+      var index = locations.indexOf(place);
+      self.populateInfoWindow(markers[index], largeInfowindow);
+    }
+} 
+
+//initialise is the callback function
+function initialise() {
+  ko.applyBindings(new ViewModel());
 
 }
+
+
